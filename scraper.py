0b0903ed9be4999a9aa72b05543f8b745717e9d9
Jane Vo <vo.janehanh@gmail.com>
2021-04-24 17:44:45 -0700

    Change valid status, ensure absolute urls

--- scraper.py
+++ scraper.py
@@ -2,12 +2,12 @@ import re
 from urllib.parse import urlparse, urldefrag, urljoin
 from bs4 import BeautifulSoup
 import requests
-from determinecrawl import should_crawl
+from determinecrawl import should_crawl, atags
 from reports import get_reports, analyze_page, tokenize
+from lxml import html
 
 def scraper(url, resp)->list:
-    ''' NEED TO DEVELOP '''
-    if 200 <= resp.status < 400:
+    if resp.status == 200:
         # no error status: return valid URLs
         links = extract_next_links(url, resp)
         get_reports()
@@ -19,34 +19,32 @@ def extract_next_links(url, resp):
     '''Finds linked webpages from a link.'''
     # the following code block is largely credited to the following documentation:
     # https://www.kite.com/python/answers/how-to-get-href-links-from-urllib-urlopen-in-python
-    relative = urlparse(url)
 
     soup = BeautifulSoup(resp.raw_response.text, 'lxml')
 
-    # check if there are too many atags compared to text (trap)
+    # check if there are too many atags compared to text (trap), or if there is no text
     if atags(url, soup):
         return []
+    
+    relative = urlparse(url)
 
     tokenize(url, soup)
     links = set()
     for link in soup.find_all('a'):
         href = link.get('href')
+        # getting rid of fragment
         href = urldefrag(href)[0]
-
-        # check if it is a relative path and if so, fill in the missing parts
+        
+        # make url relative path
         parsed = urlparse(href)
         if parsed.scheme == '' and parsed.netloc == '':
-            parsed._replace(scheme=relative.scheme)
-            parsed._replace(netloc=relative.netloc)
-        elif parsed.scheme != '' and parsed.netloc == '':
-            parsed._replace(netloc=relative.netloc)
-        elif parsed.scheme == '' and parsed.netloc != '':
-            parsed._replace(scheme=relative.scheme)
+            href = urljoin(url, href)
 
-        href = parsed.geturl()
+        #print(href)
         
         # check if this page can/should be crawled if it is a valid url
         if is_valid(href) and should_crawl(href, parsed):
+            # print('\nhecc yes\n')
             links.add(href)
             analyze_page(href)
 
@@ -66,11 +64,17 @@ def is_valid(url):
 
         if re.match(r".*\.today\.uci\.edu$", parsed.netloc) and not re.match(r".*\/department\/information_computer_sciences$", parsed.path):
             return False
+        
+        if re.match(r".*(pdf|img).*", parsed.path.lower()):
+            return False
+        
+        if re.match(r"^replytocom=.*", parsed.query.lower()):
+            return False
 
         return not re.match(    # if file extension is in the path
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ppsx|ppt"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
